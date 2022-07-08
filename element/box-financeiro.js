8cb9bebdb2688ca9e9073e76395302c9eab88b57
Diogo Carvalho <diogocarvalhofarias@gmail.com>
2022-07-08 15:22:14 -0300

    ajuste orcamento

--- element/box-financeiro.js
+++ element/box-financeiro.js
@@ -5,30 +5,84 @@ import * as Unicons from '@iconscout/react-unicons';
 function BoxFinanceiro() {
   return (
     <>
-		<section className="container">
+		<section className="container box-financeiro">
 			<div className="row">
-				<div className="col-lg-7 col-sm-7 section-head style-3 center-mob">
+				<div className="col-12 col-lg-7 col-sm-7 section-head style-3">
 					<h2 className="title">Financeiro</h2>
-					<p>Financeiro completo integrado com o orçamento e custos de produção.<br/> Controle também as despesas fixas da sua empresa e tenha todo o controle num único sistema.</p>
-					<div className="d-flex center-mob">
-						<ul>
-							<li><Unicons.UilCheck /> Conciliação bancária</li>
-							<li><Unicons.UilCheck /> Contas a pagar</li>
-							<li><Unicons.UilCheck /> Contas a receber</li>
-							<li><Unicons.UilCheck /> Faturamento</li>
-						</ul>
-					</div>
-					<div className="center-mob">
-						<ul>
-							<li><Unicons.UilCheck /> Nota Fiscal Eletrônica</li>
-							<li><Unicons.UilCheck /> Fluxo de caixa</li>
-							<li><Unicons.UilCheck /> Plano de contas</li>
+					<h5>Financeiro completo integrado com o orçamento e custos de produção.</h5>
+					<p>Controle também as despesas fixas da sua empresa e tenha todo o controle num único sistema.</p>
+					<div className="d-flex">
+						<ul className="row">
+							<li className="col-12 col-md-6 d-flex  mb-3">
+								<div>
+								<Unicons.UilArrowCircleRight />
+								</div>
+								<div className="ml-3">
+								<h6>Conciliação bancária por OFX e Arquivo CNAB</h6>
+								<span>Ganhe tempo e praticidade com cobranças bancárias automatizadas</span>
+								</div>
+							</li>
+							<li className="col-12 col-md-6 d-flex mb-3">
+								<div>
+								<Unicons.UilArrowCircleRight />
+								</div>
+								<div className="ml-3">
+								<h6>Faturamento</h6>
+								<p>lorem ipsum</p>
+								</div>
+							</li>
+							<li className="col-12 col-md-6 d-flex mb-3">
+								<div>
+								<Unicons.UilArrowCircleRight />
+								</div>
+								<div className="ml-3">
+								<h6>Contas a pagar</h6>
+								<p>Tenha total controle de todos os pagamentos, aprove-os e finalize através de um arquivo CNAB.</p>
+								</div>
+								</li>
+							<li className="col-12 col-md-6 d-flex mb-3">
+								<div>
+								<Unicons.UilArrowCircleRight />
+								</div>
+								<div className="ml-3">
+								<h6>Contas a receber</h6>
+								<p>Veja o saldo total a receber e filtre por data de vencimento ou clientes devedores.</p>
+								</div>
+							</li>
+							
+							<li className="col-12 col-md-6 d-flex mb-3">
+								<div>
+								<Unicons.UilArrowCircleRight />
+								</div>
+								<div className="ml-3">
+								<h6>Nota Fiscal Eletrônica</h6>
+								<p>Emita quantas NF-e precisar e envie para o email ou whatsapp do seu cliente.</p>
+								</div>
+							</li>
+							<li className="col-12 col-md-6 d-flex mb-3">
+								<div>
+								<Unicons.UilArrowCircleRight />
+								</div>
+								<div className="ml-3">
+								<h6>Fluxo de caixa</h6>
+								<p>Acompanhe o saldo do caixa diário, faça filtros por contas bancárias e veja o fluxo detalhado.</p>
+								</div>
+							</li>
+							<li className="col-12 col-md-6 d-flex">
+								<div>
+								<Unicons.UilArrowCircleRight />
+								</div>
+								<div className="ml-3">
+								<h6>Plano de contas</h6>
+								<p>Crie ilimitados planos de contas, personalize como achar melhor.</p>
+								</div>
+							</li>
 						</ul>
 					</div>
 				</div>
-				<div className="col-lg-5 col-sm-5">
-					<div data-aos="fade-up" data-aos-duration="400">
-						<Image src="/images/banner/losango-integracao.png" alt="" width={525} height={584} />
+				<div className="col-12 col-lg-5 col-sm-5">
+					<div data-aos="fade-up" data-aos-duration="400" className="d-flex justify-content-end">
+						<Image src="/images/banner/mulher-financeiro.png" alt="" width={525} height={719} />
 					</div>
 				</div>  
 			</div>
--- element/box-orcamento.js
+++ element/box-orcamento.js
@@ -5,23 +5,26 @@ import Link from "next/link";
 import Image from 'next/image';
 import Footer from "../layout/footer";
 import Header from "../layout/header";
+import ReactPlayer from 'react-player';
 
 function BoxOrcamento() {
     return (
       <>
 		<section className="container">
 			<div className="row align-items-center">
-				<div className="col-lg-5 col-sm-5 d-flex align-items-center justify-content-center center-mob">
-					<Image src="/images/banner/gupshup.gif" alt="" width={230} height={452} />
-				</div>
-				<div className="col-lg-7 col-sm-7">
+				
+				<div className="col-lg-5 col-sm-5">
+					<h2 className="title">Orçamento</h2>
 					<h5>O Gestor24h é o melhor sistema para gestão de orçamentos de serviços, controle de custos com fornecedores e financeiro completo.  Tudo integrado num único sistema.  </h5>
-					<p>O mais completo mas sem complicar.</p>
 					<p>Monte seu orçamento como se estivesse no excel, adicionando fornecedores, comissões, imposto, taxas e ainda calculando lucratividade ( cálculo markup aplicado) tudo de maneira simplificada e com agilidade.</p>
 					<p>Envie sua proposta por pdf ou através de um link acessível pelo seu cliente.</p>
 					<p>Controle todos os custos envolvidos, terceiros/fornecedores contratados e despesas extras, tudo integrado ao contas a pagar e receber.</p>
 					<p>Faturamento e Financeiro completo, você não precisa de outro sistema para sua gestão financeira. O Gestor24h já tem tudo num único sistema.</p>
 				</div>  
+				<div className="col-lg-7 col-sm-7 d-flex align-items-center justify-content-center center-mob">
+					{/* <ReactPlayer playing loop  url='/images/video/orcamento-1.mp4' /> */}
+					<Image src="/images/video/video.gif" alt="" width={600} height={338} />
+				</div>
 			</div>
 		</section>
 		</>
