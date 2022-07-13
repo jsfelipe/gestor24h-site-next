dfbb69c28de839a216264d9643d1da74e000a62f
Diogo Carvalho <diogocarvalhofarias@gmail.com>
2022-07-13 13:40:10 -0300

    ajuste rodape

--- layout/footer.js
+++ layout/footer.js
@@ -1,3 +1,6 @@
+
+import Link from 'next/link';
+
 function Footer() {
   return (
     <>
@@ -5,58 +8,32 @@ function Footer() {
     <footer className="site-footer" id="footer">
 		<div className="footer-top">
             <div className="container">
-				<div className="row">
-					
-					<div className="col-xl-2 col-lg-3 col-md-4  col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="0.4s">
-						<div className="widget style-1 center-mob">
-							<h5 className="footer-title">Empresa</h5>
-							<ul className="center-mob">
-								<li><a href="/sobre">Sobre</a></li>
-								<li><a href="https://blog.meets.com.br/" target="_blank">Blog</a></li>
-								<li><a href="/parceiros">Parceiros</a></li>
+				<div className="row d-flex align-items-center justify-content-center">
+
+                        <div className="">
+							<ul className="d-flex justify-content-center center-mob links  mb-3">
+								<li>
+									<Link href="/termos-de-uso/" ><a target="_blank">Termos de Uso</a></Link>
+								</li>
+								<li className='barra mob-none'>|</li>
+								<li>
+									<Link href="/seguranca/" ><a target="_blank">Segurança</a></Link>
+								</li>
 							</ul>
-						</div>
-                    </div>
-					<div className="col-xl-2 col-lg-3 col-md-4  col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="0.6s">
-						<div className="widget style-1 center-mob">
-							<h5 className="footer-title">Soluções</h5>
-							<ul className="center-mob">
-								<li><a href="/solucoes/crm-para-infoprodutos">CRM para Infoprodutos</a></li>
-								<li><a href="/solucoes/crm-para-agencias">CRM para Agências</a></li>
-								<li><a href="/solucoes/crm-para-pme">CRM para PME</a></li>
+							<ul className="dlab-social-icon d-flex justify-content-center center-mob">
+								<li><a className="fa fa-facebook" href="https://www.facebook.com/meetscrm/?ref=br_rs" target="_blank"></a></li>
+								<li><a className="fa fa-instagram" href="https://www.instagram.com/meetscrm/" target="_blank"></a></li>
+								<li><a className="fa fa-linkedin" href="https://www.linkedin.com/company/meetscrm/" target="_blank"></a></li>
+								<li><a className="fa fa-twitter" href="https://twitter.com/meets_crm" target="_blank"></a></li>
+								<li><a className="fa fa-youtube" href="https://www.youtube.com/c/MeetsBr" target="_blank"></a></li>
+								<li><a className="fa fa-pinterest" href="https://br.pinterest.com/meetscrm1/" target="_blank"></a></li>
 							</ul>
-						</div>
-                    </div>
-					<div className="col-xl-3 col-lg-3 col-md-4  col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="1.0s">
-						<div className="widget style-1 center-mob">
-							<h5 className="footer-title">Suporte</h5>
-							<ul className="center-mob">
-								<li><a href="https://ajuda.meets.com.br/" target="_blank">Ajuda</a></li>
-								<li><a href="mailto:contato@meets.com.br">contato@meets.com.br</a></li>
-								<li><a href="tel:+551131649266">+55 (11) 3164-9266</a></li>
-								<li><a href="tel:+5581992971917">+55 (81) 99297-1917</a></li>
-							</ul>
-						</div>
-					</div>
-					<div className="col-xl-3 col-lg-12 col-md-4 col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="0.2s">
-                        <div className="widget_about">
-							<div className="dlab-social-icon center-mob">
-								<h5 className="footer-title">Social</h5>
-								<ul className="center-mob">
-									<li><a className="fa fa-facebook" href="https://www.facebook.com/meetscrm/?ref=br_rs" target="_blank"></a></li>
-									<li><a className="fa fa-instagram" href="https://www.instagram.com/meetscrm/" target="_blank"></a></li>
-									<li><a className="fa fa-linkedin" href="https://www.linkedin.com/company/meetscrm/" target="_blank"></a></li>
-									<li><a className="fa fa-twitter" href="https://twitter.com/meets_crm" target="_blank"></a></li>
-									<li><a className="fa fa-youtube" href="https://www.youtube.com/c/MeetsBr" target="_blank"></a></li>
-									<li><a className="fa fa-pinterest" href="https://br.pinterest.com/meetscrm1/" target="_blank"></a></li>
-								</ul>
-							</div>
-							<div className="footer-logo center-mob">
-								<img src="images/logo/aws.png" alt=""/>
-								<img src="images/logo/google-safe-browsing.png" alt=""/>
+							
+							<div className="footer-logo align-items-center justify-content-center center-mob">
+								<img src="images/logo/aws.png" alt="" className='mr-3' />
+								<img src="images/logo/google-safe-browsing.png" alt="" className='google' />
 							</div>
 						</div>
-                    </div>
                 </div>
             </div>
 		</div>
