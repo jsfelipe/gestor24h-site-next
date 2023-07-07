4a8a09d8a5fbc3384200af6adb52972cc5fd7ecb
Sandro Cavalcanti <sandrocavalcanti@gmail.com>
2023-07-07 21:22:04 +0100

    Update footer.js

--- layout/footer.js
+++ layout/footer.js
@@ -23,13 +23,13 @@ function Footer() {
 							<ul className="dlab-social-icon d-flex justify-content-center center-mob">
 								<li><a className="fa fa-facebook" href="https://www.facebook.com/gestor24h/" target="_blank"></a></li>
 								<li><a className="fa fa-instagram" href="https://instagram.com/gestor24h" target="_blank"></a></li>
-								{/* <li><a className="fa fa-linkedin" href="https://www.linkedin.com/company/" target="_blank"></a></li> */}
-								<li><a className="fa fa-twitter" href="https://twitter.com/" target="_blank"></a></li>
+								<li><a className="fa fa-linkedin" href="https://www.linkedin.com/company/gestor24h" target="_blank"></a></li>
+								{/* <li><a className="fa fa-twitter" href="https://twitter.com/" target="_blank"></a></li> */}
 								<li><a className="fa fa-youtube" href="https://www.youtube.com/channel/UCwYBI8UnzVB1ryq1fILBl_Q" target="_blank"></a></li>
 								
 							</ul>
 							<div className="footer-logo align-items-center justify-content-center center-mob">
-								<a href="https://api.whatsapp.com/send?phone=5581994994603" target="_blank" data-aos="fade-up" data-aos-duration="300" className="btn btn-light btn-round"><i className="fa fa-whatsapp fa-lg"></i> Whatsapp</a>
+								<a href="https://api.whatsapp.com/send?phone=5581994994603" target="_blank" data-aos="fade-up" data-aos-duration="300" className="btn btn-light btn-round"><i className="fa fa-whatsapp fa-lg"></i> WhatsApp</a>
 						</div>
 							
 							<div className="footer-logo align-items-center justify-content-center center-mob">
@@ -45,16 +45,12 @@ function Footer() {
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-md-12 text-center"> 
-						<span className="copyright-text">Copyright © 2022 <a href="https://dexignzone.com/" target="_blank">Gestor 24h</a>. Todos os direitos reservados.</span> 
+						<span className="copyright-text">Copyright © 2023 Gestor24h. Todos os direitos reservados.</span> 
 					</div>
                 </div>
             </div>
         </div>
 		
-		<noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GJQK9J"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
-		
-	
     </footer>
     {/* <!-- Footer End --> */}
     </>
