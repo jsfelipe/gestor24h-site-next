95cb2f1dd3aaf6d9065d283ad47360caaa81fc2c
Sandro Cavalcanti <sandrocavalcanti@gmail.com>
2023-07-07 21:26:22 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -69,11 +69,11 @@ function MyApp({ Component, pageProps })  {
       <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
       <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
       
-			<title>Gestor 24h - Sistema de Gestão para Serviços, orçamentos e financeiro</title>
-      <meta property="og:description" content="Gestor 24h - Sistema de Gestão para Serviços, orçamentos e financeiro" />
-      <meta name="description" content="Gestor 24h - Sistema de Gestão para Serviços, orçamentos e financeiro" />
+			<title>Gestor24h - Sistema de Gestão para Serviços, orçamentos e financeiro</title>
+      <meta property="og:description" content="Gestor24h - Sistema de Gestão para Serviços, orçamentos e financeiro" />
+      <meta name="description" content="Gestor24h - Sistema de Gestão para Serviços, orçamentos e financeiro" />
       <meta property="og:url" content="https://meets.com.br/" />
-      <meta property="og:site_name" content="Gestor 24h - Gestão de Serviços Online" />
+      <meta property="og:site_name" content="Gestor24h - Gestão de Serviços Online" />
 			<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png"/>
       
       <script
@@ -86,33 +86,14 @@ function MyApp({ Component, pageProps })  {
       
       
       
-      {/* <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=UA-2333523-28`}
-          />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'UA-2333523-28', {
-            page_path: window.location.pathname,
-          });
-        `,
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5GJQK9J');
-        `,
-          }}
-        /> */}
+      <!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-2333523-57%22%3E</script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-2333523-57');
+	</script>
 
 
      
