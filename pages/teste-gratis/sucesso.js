7edf5460cc86a5d9a6ec5f78e3f52991dd48e3d4
Sandro Cavalcanti <sandrocavalcanti@gmail.com>
2023-07-17 16:07:45 +0100

    Update sucesso.js

--- pages/teste-gratis/sucesso.js
+++ pages/teste-gratis/sucesso.js
@@ -21,7 +21,7 @@ function Sucesso() {
                       {/* <img src="../images/icon/check.png"  /> */}
                       <h2 className="text-white">Seu Cadastro foi Realizado com Sucesso!</h2>
                       <h5 className="text-primary">Clique no link abaixo para realizar seu login e começar a usar o Gestor24h.</h5>
-                      <a href="https://app.gestor24h.com.br/" className="btn btn-primary border-0 mt-3">Faça seu login</a>
+                      <a href="https://appv2.gestor24h.com.br/" className="btn btn-primary border-0 mt-3">Faça seu login</a>
                       
                     </div>
                 </div>
