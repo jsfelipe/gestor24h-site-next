0824d9d20425a4991da60e5d2dd636ba03d2bed7
Felipe <jsfelipe@gmail.com>
2023-07-17 12:03:46 -0300

    login

--- layout/header.js
+++ layout/header.js
@@ -92,14 +92,14 @@ function Header() {
 									<li><Link href="#box-recursos"><a>Recursos</a></Link></li>
                                     <li><Link href="#vantagens"><a>Vantagens</a></Link></li>
                                     <li><Link href="#box-planos"><a>Planos</a></Link></li>
-                                    {/* <li><Link href="https://ajuda.meets.com.br" ><a target="_blank" rel="noreferrer" >Ajuda</a></Link></li> */}
+                                    
                                 </ul>
                                  <div className="dlab-social-icon">
                                     <div className="extra-cell d-flex flex-column">
                                         <Link href="teste-gratis">
                                             <a className="btn btn-primary rounded-xl shadow" target="_blank" rel="noreferrer" >Solicitar teste grátis</a>
                                         </Link>
-                                        <Link href="https://app.gestor24h.com.br/">
+                                        <Link href="https://www.gestor24h.com.br/login">
                                             <a className="btn btn-login rounded-xl border border-white border-5" target="_blank" rel="noreferrer">Login</a>
                                         </Link>
                                     </div>
