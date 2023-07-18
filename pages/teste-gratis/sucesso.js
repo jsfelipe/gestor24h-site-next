b5b24da56f8a28f3a94f30bcda7a0b9b4a2a5846
Felipe <jsfelipe@gmail.com>
2023-07-18 18:44:15 -0300

    texto sucesso

--- pages/teste-gratis/sucesso.js
+++ pages/teste-gratis/sucesso.js
@@ -20,7 +20,7 @@ function Sucesso() {
                     <div className="sucesso d-flex align-items-center justify-content-center flex-wrap col-md-5 p-5 text-center">
                       {/* <img src="../images/icon/check.png"  /> */}
                       <h2 className="text-white">Seu Cadastro foi Realizado com Sucesso!</h2>
-                      <h5 className="text-primary">Clique no link abaixo para realizar seu login e começar a usar o Gestor24h.</h5>
+                      <h5 className="text-primary">Verifique seu e-mail com o login e senha e clique no link abaixo para realizar seu login e começar a usar o Gestor24h.</h5>
                       <a href="https://appv2.gestor24h.com.br/" className="btn btn-primary border-0 mt-3">Faça seu login</a>
                       
                     </div>
